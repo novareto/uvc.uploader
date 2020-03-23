1856cd836dd351f5dfb7be6794a5021aff42c72b
Christian Klinger <ck@novareto.de>
2020-03-23 09:53:28 +0100

    changes

--- uvc/uploader/views.py
+++ uvc/uploader/views.py
@@ -2,40 +2,40 @@
 # # Copyright (c) 2007-2019 NovaReto GmbH
 # # cklinger@novareto.de
 
-import grok
-import uvcsite
-
-from zope import interface, schema
-from .field import FilesField
-from dolmen.file.file import NamedFile
-
-
-class IPerson(interface.Interface):
-
-    name = schema.TextLine(
-        title=u"Name",
-        description=u"Give us the Name",
-    )
-
-    files = FilesField(
-        title=u"Files",
-        description=u"Please Upload some Files here",
-    )
+#import grok
+#import uvcsite
+#
+#from zope import interface, schema
+#from .field import FilesField
+#from dolmen.file.file import NamedFile
 
 
-class PersonForm(uvcsite.Form):
-    grok.name('pf')
-    grok.context(uvcsite.IUVCSite)
+#class IPerson(interface.Interface):
+#
+#    name = schema.TextLine(
+#        title=u"Name",
+#        description=u"Give us the Name",
+#    )
+#
+#    files = FilesField(
+#        title=u"Files",
+#        description=u"Please Upload some Files here",
+#    )
 
-    fields = uvcsite.Fields(IPerson)
 
-    @uvcsite.action('Save')
-    def handle_save(self):
-        data, errors = self.extractData()
-        files = data.pop('files')
-        if files is not None:
-            fobjs = [NamedFile(data=fd, filename=fd.filename)
-                     for fd in files]
-
-        print data, fobjs
-        print errors
+#class PersonForm(uvcsite.Form):
+#    grok.name('pf')
+#    grok.context(uvcsite.IUVCSite)
+#
+#    fields = uvcsite.Fields(IPerson)
+#
+#    @uvcsite.action('Save')
+#    def handle_save(self):
+#        data, errors = self.extractData()
+#        files = data.pop('files')
+#        if files is not None:
+#            fobjs = [NamedFile(data=fd, filename=fd.filename)
+#                     for fd in files]
+#
+#        print data, fobjs
+#        print errors
