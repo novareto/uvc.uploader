054d4b752e5ff601cfa6488108e7d6c3d8014bec
Christian Klinger <ck@novareto.de>
2020-08-27 13:09:30 +0200

    changes

--- uvc/uploader/field.py
+++ uvc/uploader/field.py
@@ -1,4 +1,4 @@
-import grokcore.view as grok
+import grok
 
 from dolmen.widget.file import MF as _
 from zeam.form.base import interfaces, NO_VALUE, NO_CHANGE
@@ -11,6 +11,8 @@ from zope.size.interfaces import ISized
 from zope.schema import Field
 from zope.interface import implementer, Interface
 from .resources import filer_css, filer_js
+from persistent.list import PersistentList
+from dolmen.file import NamedFile
 
 
 grok.templatedir('templates')
@@ -56,10 +58,27 @@ class FilesWidget(SchemaFieldWidget):
         filer_css.need()
         filer_js.need()
 
-        if not self.form.ignoreContent:
-            raise NotImplementedError("Files can't be represented.")
+        #if not self.form.ignoreContent:
+        ##    raise NotImplementedError("Files can't be represented.")
 
         SchemaFieldWidget.update(self)
+
+    def getFiles(self):
+        import json
+        content = self.form.getContentData().getContent()
+        if getattr(content, self.component._field.getName(), None):
+            rc = [{
+                'url': '%s/++download++%s' % (
+                    grok.url(self.request, content),
+                    fileobj.filename),
+                'name': fileobj.filename,
+                'size': fileobj.size,
+                'type': fileobj.contentType
+            } for fileobj in self.component._field.get(content)]
+        else:
+            rc = []
+        print json.dumps(rc)
+        return json.dumps(rc)
 
 
 class FileWidgetExtractor(WidgetExtractor):
@@ -68,13 +87,18 @@ class FileWidgetExtractor(WidgetExtractor):
     grok.adapts(FilesSchemaField, interfaces.IFormData, Interface)
 
     def extract(self):
+        content = self.form.getContentData().getContent()
+        existing = getattr(content, self.component._field.getName())
         value = self.request.form.get(self.identifier + '[]') or NO_VALUE
         if not isinstance(value, list):
             if value is NO_VALUE:
                 value = []
             else:
                  value = [value]
-        return (value, None)
+        existing.extend([
+            NamedFile(x.read(), filename=x.filename) for x in value
+        ])
+        return (existing, None)
 
 
 def register():
